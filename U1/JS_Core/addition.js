8fcb7f93a539dbbf43b5bc7724fe546b02019716
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-03-25 21:24:01 +0530

    init

---
+++ U1/JS_Core/addition.js
@@ -0,0 +1,7 @@
+var a= Number(prompt("What is the val of 'a'? \n"));
+var b= Number(prompt("What is the val of 'b'? \n"));
+var c= Number(prompt("What is the val of 'c'? \n"));
+
+var add= a+b+c;
+
+document.write("addition of the numbers is="+add);
