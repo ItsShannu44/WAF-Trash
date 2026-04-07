8b0998da531cffa3ba53feb85f4c0896ec2dc11b
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 15:39:42 +0530

    initial commit

--- U2/Node/Array_Object_Destructing.js
+++ U2/Node/Array_Object_Destructing.js
@@ -1,7 +1,11 @@
 function getScores(){
     return[70,80,90];
 }
-
+//ES5
 let scores=getScores();
 let x=scores[0],y=scores[1],z=scores[2];
 console.log(x, y, z);
+
+//ES6
+let[a,b,c]=getScores();
+console.log(a, b, c)
