a88602a785b9705881c9450f98fe778826ad01bf
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 15:15:03 +0530

    initial commit

---
+++ U2/Node/template_literals.js
@@ -0,0 +1,14 @@
+//Multiline strings
+const text= `This is line 1
+This is line 2 
+This is line 3`;
+console.log(text);
+
+//can insert variables using ${}:
+let name="shannu";
+let msg= `Hello ${name}`;
+console.log(msg); 
+
+//Expressions inside strings
+let a=5, b=3;
+console.log(`Sum is ${a + b}`);
