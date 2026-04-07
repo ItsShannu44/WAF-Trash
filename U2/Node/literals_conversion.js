1632dea3e8d48c09cbad0e032b393d03bb41005b
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 15:15:08 +0530

    commit

---
+++ U2/Node/literals_conversion.js
@@ -0,0 +1,27 @@
+let a=051;
+console.log(a); //41
+
+let b=058;  //Invalid octal
+console.log(b); //58
+
+let c=0o51;
+console.log(c); //41
+
+let d=parseInt('111',2);
+console.log(d); //7
+
+let e=0b111;
+console.log(e); //7
+
+let f=0x10;
+console.log(f); //16
+
+let simple= `This is a template literal`;
+console.log(simple);
+
+let x= 0b1011;
+console.log(x); //11
+
+let y=0x1A;
+console.log(y); //26
+
