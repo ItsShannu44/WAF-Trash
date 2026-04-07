230ceda63226e20702884f518d5fa2344d110c71
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 14:45:50 +0530

    initial commit

--- U2/Node/1.js
+++ U2/Node/1.js
@@ -23,3 +23,32 @@ function sum(a,b,c){
 const numbers=[10,20,30];
 sum(...numbers);
 
+
+
+
+
+// Object Literal
+function createMachine(name, status){
+    return{
+        name: name,
+        status: status
+    };
+}
+console.log(createMachine("PESu",true))
+
+const m1={
+    name: "Drill",
+    status:"Active"
+};
+console.log(m1);
+
+const m2={
+    name:"Lathe",
+    status:"Inactive"
+};
+console.log(m2);
+
+// With function(dynamic creation)
+
+const m3= createMachine("Drill", "Active");
+const m4=createMachine("Lathe", "InActive");
