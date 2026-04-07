07ea0175caff755b49b07a508ee653e29e89fdd3
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 15:19:23 +0530

    initial commit

---
+++ U2/Node/string_length.js
@@ -0,0 +1,7 @@
+let simple = ` This is template literal`;
+console.log(simple);
+console.log(simple.length); //25
+console.log(typeof simple); //string
+// const text= "this is line1
+// This is line2
+// This is line3";
