0d18272cb79f407d97de249efd65ad76bd9f2f5d
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 14:24:59 +0530

    initial commit

--- U2/Node/1.js
+++ U2/Node/1.js
@@ -1,3 +1,7 @@
 // spread Operator
 
 const odd=[1,3,5];
+const combined=[2,4,6, ...odd];
+console.log(combined);
+// const combined=[...odd,2,4,6];   SyntaxError: Identifier 'combined' has already been declared
+
