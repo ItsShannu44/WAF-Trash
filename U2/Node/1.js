e970aa2d4c7fcbd591f293eca260e40bdd8cc8cb
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-07 14:31:04 +0530

    initial commit

--- U2/Node/1.js
+++ U2/Node/1.js
@@ -6,3 +6,20 @@ console.log(combined);
 // const combined=[...odd,2,4,6];   SyntaxError: Identifier 'combined' has already been declared
 const combined_1=[...odd,2,4,6]; 
 console.log(combined_1);
+
+
+const a=[1,2];
+const b=[3,4];
+const res=[...a,...b]; // [1,2,3,4]
+console.log(res);
+
+const og=[1,2,3];
+const newarr=[...og];
+console.log(newarr);
+
+function sum(a,b,c){
+    console.log(a + b+ c);
+}
+const numbers=[10,20,30];
+sum(...numbers);
+
