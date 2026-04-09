84c3fbcdbe12179147ddaa0e515b12722566c351
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-09 10:10:41 +0530

    init commit

---
+++ U2/Node/Arrow_func.js
@@ -0,0 +1,14 @@
+let add = function(x,y)
+{
+    return x+y;
+};
+console.log(add(10,20));
+
+let addition=(x,y) =>x+y;
+console.log(addition(10,50));
+
+
+// function Person(name)
+// {
+//     this.name=name;
+// }
