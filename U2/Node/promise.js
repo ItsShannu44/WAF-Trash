859e590486289309c0673d2ea16ed81c9d3a71c5
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-13 15:27:35 +0530

    init

--- U2/Node/promise.js
+++ U2/Node/promise.js
@@ -1,11 +1,17 @@
-let join={name: 'john Doe', age: 23},lily={name: 'Lily Bush'}, peter={name:'Peter Drucker'};
+let john = { name: 'John Doe', age: 23},
+lily = { name: 'Lily Bush'},
+peter = { name: 'Peter Drucker'};
 
-let userRoles=new Map([[john, 'admin'],[lily, 'editor'],[peter, 'subscriber'],]);``
-for(const [user, role]of userRoles)
-{
-    console.log(`${user.name}: ${role}`);
+let userRoles = new Map([
+    [john, 'admin'],
+    [lily, 'editor'],
+    [peter, 'subscriber'],
+]);
+
+for (const [user, role] of userRoles) {
+    console.log(`${user.name} : ${role}`);
 }
-for (let role of userRoles.values())
-{
+
+for (let role of userRoles.values()) {
     console.log(role);
-}   
+}
