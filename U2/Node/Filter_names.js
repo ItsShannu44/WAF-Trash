aa437c9e5eefa8c536c9224123ff112070b61470
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-13 15:31:21 +0530

    init

--- U2/Node/Filter_names.js
+++ U2/Node/Filter_names.js
@@ -3,3 +3,10 @@ function filterNames(names, letter) {
 }
 
 console.log(filterNames(['Rameshwara', 'Malikarjuna', 'Prakruthi', 'Namratha', 'Manohar'], 'M'));
+
+function sortByAge(arr)
+{
+    return arr.sort((a,b)=>a.age - b.age);
+}
+const people=[{ name: 'Rameshwar', age: 21},{name: 'Manohar', age: 20},{ name: 'Prakruthi', age: 23}];
+console.log(sortByAge(people));
