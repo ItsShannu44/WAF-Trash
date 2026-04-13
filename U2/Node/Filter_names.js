fd2feb37da7a8861a0571af84bc71debe0f2b1e2
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-13 15:27:51 +0530

    init

---
+++ U2/Node/Filter_names.js
@@ -0,0 +1,5 @@
+function filterNames(names, letter) {
+    return names.filter(name => name.startsWith(letter));
+}
+
+console.log(filterNames(['Rameshwara', 'Malikarjuna', 'Prakruthi', 'Namratha', 'Manohar'], 'M'));
