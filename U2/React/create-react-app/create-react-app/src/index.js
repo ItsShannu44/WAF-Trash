10f44594f285831b459667a5a6df6aeb97ac5141
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-16 10:29:02 +0530

    initial commit

--- U2/React/create-react-app/create-react-app/src/index.js
+++ U2/React/create-react-app/create-react-app/src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Greeting from './Greeting';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App/></React.StrictMode>);
+/*
+const x=5;
+let text="Goodbye";
+if(x<10)
+{
+  text= "Hello from React.. What's that reaction!!!";
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const myElement= <h1>{text}</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
+*/
+
+// const myElement= <h1>I like Jsx..</h1>;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
+
+// const x=50;
+// const myElement= <h1>{(x) <10 ? "Hello React" : "Goodbye React"}</h1>;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
 reportWebVitals();
