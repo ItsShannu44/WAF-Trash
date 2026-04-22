16aa469e9c30d6401bf8f606b34098309d1c70ad
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-22 17:47:41 +0530

    changed initial file

---
+++ U2/React/create-react-app/create-react-app/src/AppDynamic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  Booktable from './BookTable';
+
+function App()
+{
+    const book=[
+        {
+            isbn: '978-715-938-833-33',
+            title: "new book"
+        }
+    ]
+}
