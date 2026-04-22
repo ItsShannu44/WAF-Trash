7261b5e12b98e20de567e4b2bad7a4795660a0fb
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-22 15:00:52 +0530

    init commit

--- U2/React/create-react-app/create-react-app/src/index.js
+++ U2/React/create-react-app/create-react-app/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Greeting from './Greeting';
-
+// import Greeting from './Greeting';
+import App from './Appcomposing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App/></React.StrictMode>);
+root.render(<App/>);
+// root.render(<React.StrictMode><App/></React.StrictMode>);
 /*
 const x=5;
 let text="Goodbye";
