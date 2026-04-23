01f02216a34f210016be6db2bdab4318126d3510
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-23 10:46:18 +0530

    init changes

--- U2/React/create-react-app/create-react-app/src/AppDynamic.js
+++ U2/React/create-react-app/create-react-app/src/AppDynamic.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import  Booktable from './BookTable';
+import  BookTable from './components/BookTable';
 
 function App()
 {
-    const book=[
+    const books=[
         {
             isbn: '978-715-938-833-33',
-            title: "new book"
+            title: "new book",
+            author: "Vaibhav",
+            price: 499,
+            published: new Date('2017-11-30')
+        },
+        {
+            isbn: '978-715-938-833-33',
+            title: "Hell or Heaven",
+            author: "Aniston",
+            price: 569,
+            published: new Date('2019-08-25')
+        },
+        {
+            isbn: '978-715-938-833-33',
+            title: "Fairy Tales",
+            author: "Martin Ross",
+            price: 999,
+            published: new Date('2020-07-16')
         }
-    ]
+    ];
+    return(
+        <div className='App'>
+            <header className='App-header'>
+                <h3 style={{textAlign:"center", fontSize:"40px", color:"green"}}>Book Store</h3>
+            </header>
+            <BookTable books={books}/>
+        </div>
+    )
 }
+export default App;
