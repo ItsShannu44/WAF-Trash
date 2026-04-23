60525be33250c01e196f0aadcaf4014574f18c4f
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-23 10:48:51 +0530

    init changes

---
+++ U2/React/create-react-app/create-react-app/src/components/BookClass.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+// import './App.css';
+
+function BookClass(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <table border={2}>
+                    <tr>
+                        <th>Sunday</th>
+                        <td>AAD</td>
+                        <td>WAF</td>
+                    </tr>
+                </table>
+
+            </header>
+        </div>
+    );
+}
+export default BookClass;
