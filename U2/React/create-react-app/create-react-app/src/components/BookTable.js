243bc2ebfa64ac89a09e476ac542fb16ef397eae
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-04-23 10:49:32 +0530

    init changes

---
+++ U2/React/create-react-app/create-react-app/src/components/BookTable.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BookRow from './BookRow';
+
+function BookTable({books})
+{
+    const bookRows=books.map(book =>(<BookRow key={book.isbn} book={book}/>));
+    return(
+        <table border="2" align='center'>
+            <thead>
+                <tr>
+                    <th>ISBN</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Price</th>
+                    <th>Published</th>
+                </tr>
+            </thead>
+            <tbody>
+                {bookRows}
+            </tbody>
+        </table>
+    );
+}
+export default BookTable;
