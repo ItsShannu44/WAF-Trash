8c71e2b3be98961f9980cf5fbcc48bf8cd2907ca
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-14 10:37:07 +0530

    updates

--- u3/src/index.js
+++ u3/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './App_Child_to_Parent'
 
 import reportWebVitals from './reportWebVitals';
 
