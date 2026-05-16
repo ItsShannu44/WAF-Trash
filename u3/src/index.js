96771a220dceb15e8945d71d5563632d6a1897a2
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-16 22:03:14 +0530

    initial changes

--- u3/src/index.js
+++ u3/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 // import App from './components/Increment_Decrement'
 // import App from './components/Dynamic_Row'
 // import App from './components/Add_Btn_Event'
-
+import App from './components/Lifting_Up'
 
 import reportWebVitals from './reportWebVitals';
 
