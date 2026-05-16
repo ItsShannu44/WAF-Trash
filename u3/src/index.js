5eb69ce1c3306221b78a4d58eca4746f26a9cb44
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-16 22:19:17 +0530

    initial changes

--- u3/src/index.js
+++ u3/src/index.js
@@ -6,7 +6,10 @@ import './index.css';
 // import App from './components/Add_Btn_Event'
 // import App from './components/Lifting_Up'
 // import App from './components/Student_Attendance'
-import App from './components/Shopping_Cart'
+// import App from './components/Shopping_Cart'
+// import App from './components/Directory_Filter'
+// import App from './components/Movie_Review'
+import App from './components/Temperature_Converter'
 
 import reportWebVitals from './reportWebVitals';
 
