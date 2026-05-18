c5d43f20e01ffcfe596eeac27907bdac9b38745a
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-18 15:52:38 +0530

    updated version

---
+++ U3/simple_routing/src/components/Aboutus.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About=() =>{
+    return(
+        <div className='container' style={pageStyle}>
+            <h4 className='center'>Home</h4>
+            <p>My home page content is here</p>
+        </div>
+    );
+};
+const pageStyle={
+    background: 'skyblue',
+    color:'maroon',
+    padding: 'o.10em'
+};
+export default About;
