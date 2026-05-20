2b8836d0beb4f7b1c7c97c719c23462e4fe6443a
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-20 15:12:20 +0530

    updated version

---
+++ U3/simple_routing/src/Student_App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Stu_Home";
+import Student from "./components/Student";
+
+function App()
+{
+    
+}
