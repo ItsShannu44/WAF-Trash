b1fd53ea3136ccd39b337e191298ecc71cbeb2ee
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-20 15:16:15 +0530

    updated version

--- U3/simple_routing/src/Student_App.js
+++ U3/simple_routing/src/Student_App.js
@@ -5,5 +5,17 @@ import Student from "./components/Student";
 
 function App()
 {
-    
+    return(
+        <BrowserRouter>
+            <h1>Student Record System</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/student/101">SRN 101</Link>
+                <Link to="/student/102">SRN 102</Link>
+                <Link to="/student/103">SRN 103</Link>
+            </nav>
+            <hr/>
+            
+        </BrowserRouter>
+    );
 }
