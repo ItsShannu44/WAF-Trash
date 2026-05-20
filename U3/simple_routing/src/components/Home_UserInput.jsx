2c05783f59956e5b82438f21ce6a5cd770cdd149
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-20 15:59:16 +0530

    updated version

--- U3/simple_routing/src/components/Home_UserInput.jsx
+++ U3/simple_routing/src/components/Home_UserInput.jsx
@@ -4,5 +4,15 @@ import { useNavigate } from "react-router-dom";
 function Home()
 {
     const[id, setId]= useState('');
-    
+    const navigate= useNavigate();
+    const handleSearch =() =>
+    {
+        if(id.trim()!=="")
+        {
+            navigate(`/student/${id}`);
+        }
+    };
+    return(
+        
+    );
 }
