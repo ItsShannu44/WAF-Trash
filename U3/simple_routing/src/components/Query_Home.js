94ce8f5f73b34c27070ce32ee299076161b4e0be
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-21 10:16:36 +0530

    new changes

---
+++ U3/simple_routing/src/components/Query_Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function New_Home()
+{
+    return(
+        <div style={pageStyle}>
+            <center>
+            <h2>Home Page</h2>
+            <p>Select Student Id to view details.</p>
+            </center>
+        </div>
+    );
+}
+const pageStyle={
+    background: 'green',
+    height:'50vh',
+    color:'white',
+    justifycontent:'center',
+    padding: '0.10rem'
+};
+export default New_Home;
