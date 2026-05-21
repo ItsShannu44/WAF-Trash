3fe87d7907ba4c6ed049cd5723aa9a804f424016
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-21 10:16:28 +0530

    new changes

--- U3/simple_routing/src/components/Query_Student.js
+++ U3/simple_routing/src/components/Query_Student.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { useSearchParams, setSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-function Student()
-{
-    const [useSearchParams, setSearchParams]= useSearchParams();
-    const id= searchParams.get("id");
-    const name= searchParams.get("name");
+function Student() {
 
-    const changeStudent =() =>{
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    const name = searchParams.get("name");
+
+    const changeStudent = () => {
         setSearchParams({
-            id:104,
-            name:"Mike"
+            id: 104,
+            name: "Mike"
         });
     };
-    return(
+
+    return (
         <div className="container" style={pageStyle}>
+            <center>
             <h2>Student Details</h2>
-            <p><b>Student Id:</b>{id}</p>
-            <p><b>Student Name:</b>{name}</p>
-            <button onClick={changeStudent}>Load SRN 104</button>
+
+            <p><b>Student Id:</b> {id}</p>
+            <p><b>Student Name:</b> {name}</p>
+
+            <button onClick={changeStudent}>
+                Load SRN 104
+            </button>
+            </center>
         </div>
     );
 }
-const pageStyle={
+
+const pageStyle = {
     background: 'skyblue',
-    color:'black',
-    padding:'0.10em'
+    color: 'black',
+    height:'50vh',
+    padding: '0.10em',
 };
 
 export default Student;
