6f1f511c3878052a55096b01922a6c2310796362
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-21 10:01:07 +0530

    new changes

--- U3/simple_routing/src/components/Query_Student.js
+++ U3/simple_routing/src/components/Query_Student.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { useSearchParams, useSearchParams } from "react-router-dom";
+import { useSearchParams, setSearchParams } from "react-router-dom";
 
 function Student()
 {
@@ -14,6 +13,19 @@ function Student()
             name:"Mike"
         });
     };
-    
+    return(
+        <div className="container" style={pageStyle}>
+            <h2>Student Details</h2>
+            <p><b>Student Id:</b>{id}</p>
+            <p><b>Student Name:</b>{name}</p>
+            <button onClick={changeStudent}>Load SRN 104</button>
+        </div>
+    );
 }
+const pageStyle={
+    background: 'skyblue',
+    color:'black',
+    padding:'0.10em'
+};
+
 export default Student;
