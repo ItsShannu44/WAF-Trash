abde3138ea3ad8d8d53cba1f8eaee007f0a7dfdc
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-27 15:35:24 +0530

    init

--- U3/express_routing/index_2.js
+++ U3/express_routing/index_2.js
@@ -1,24 +1,31 @@
-var express= require('express');
-var app=express();
-var PORT=3000;
+var express = require('express');
+var app = express();
 
-app.post('/', function(req, res){
-    console.log('Got a post request for the homepage');
+var PORT = 3000;
+
+// POST Request
+app.post('/', function(req, res) {
+    console.log('Got a POST request for the homepage');
     res.send('Post Page');
 });
 
-app.delete('/del_student', function(req, res){
-    console.log('Got a delete request for the delete page');
+// DELETE Request
+app.delete('/del_student', function(req, res) {
+    console.log('Got a DELETE request for the delete page');
     res.send('The Page got deleted');
 });
 
-app.get('/enrolled_student', function(req, res){
-    console.log('Got a get request from enrolled student page');
+// GET Request
+app.get('/enrolled_student', function(req, res) {
+    console.log('Got a GET request from enrolled student page');
     res.send('I am enrolled student');
 });
 
-var server= app.listen(3000,function(){
-    var host=server.address().address
-    var port=server.address().port
-    console.log('Example app listening at http://%s:%s',host,port)
-})
+// Server
+var server = app.listen(PORT, function() {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
