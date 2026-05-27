416d253d9b68459050d9ccb8b3f800e291efeeb9
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-27 15:28:31 +0530

    init

---
+++ U3/express_routing/index_2.js
@@ -0,0 +1,23 @@
+var express= require('express');
+var app=express();
+
+app.post('/', function(req, res){
+    console.log('Got a post request for the homepage');
+    res.send('Post PAge');
+});
+
+app.delete('/del_student', function(req, res){
+    console.log('Got a delete request for the delete page');
+    res.send('The Page got deleted');
+});
+
+app.get('/enrolled_student', function(req, res){
+    console.log('Got a get request from enrolled student page');
+    res.send('I am enrolled student');
+});
+
+var server= app.listen(3000,function(){
+    var host=server.address().address
+    var port=server.address().port
+    console.log('Example app listening at http://%s:%s',host,port);
+})
