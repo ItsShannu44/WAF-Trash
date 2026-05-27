5240563de110e49aaa07ccda803500b3e72ce8fe
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-27 15:16:21 +0530

    init

--- U3/express_routing/new_index.js
+++ U3/express_routing/new_index.js
@@ -1,19 +1,43 @@
+// const express= require('express');
+// const app = express();
+// const PORT = 3000;
+
+// const router = express.Router();
+// router.get('/', (req, res)=> {
+//     console.log("Express Router is working");
+//     res.send("Express Router is Working");
+// });
+
+// app.use(router);
+
+// app.listen(PORT, (err)=>{
+//     if(err){
+//         console.error("Error starting server:", err);
+//     }else{
+//         console.log(`Server listening on PORT ${PORT}`);
+//     }
+// });
+
+
 const express= require('express');
 const app = express();
 const PORT = 3000;
 
-const router = express.Router();
-router.get('/', (req, res)=> {
-    console.log("Express Router is working");
-    res.send("Express Router is Working");
+const router0= express.Router();
+const router1= express.Router();
+const router2= express.Router();
+const router3= express.Router();
+
+router0.get('/', function( req, res, next){
+    console.log("Home Page for section A");
+    res.send("Home Page for section A")
+});
+router1.get('/user', function( req, res, next){
+    console.log("Home Page for section B");
+    res.send("Home Page for section B")
 });
 
-app.use(router);
-
-app.listen(PORT, (err)=>{
-    if(err){
-        console.error("Error starting server:", err);
-    }else{
-        console.log(`Server listening on PORT ${PORT}`);
-    }
+router2.get('/', function( req, res, next){
+    console.log("Home Page for section C");
+    res.send("Home Page for section C")
 });
