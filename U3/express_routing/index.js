b427b66c2287426e7ca86947532a60ed42760ad9
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-27 14:59:47 +0530

    init

--- U3/express_routing/index.js
+++ U3/express_routing/index.js
@@ -2,7 +2,7 @@ const express= require("express");
 const app= express();
 
 app.get("/", (req,res)=>{
-    res.send("this is Home Page");
+    res.send("This is Home Page");
 });
 app.get("/about",(req,res)=>{
     res.send("About Page");
