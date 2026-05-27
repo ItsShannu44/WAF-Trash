4c692a698f254e31ade0e68bbf6c6db5c738b0e4
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-27 15:40:38 +0530

    new updates

--- U3/express_routing/index_2.js
+++ U3/express_routing/index_2.js
@@ -1,31 +1,23 @@
-var express = require('express');
-var app = express();
+var express= require('express');
+var app=express();
+var PORT=3000;
 
-var PORT = 3000;
-
-// POST Request
-app.post('/', function(req, res) {
-    console.log('Got a POST request for the homepage');
+app.get('/', function(req, res){
+    console.log('Got a post request for the homepage');
     res.send('Post Page');
 });
 
-// DELETE Request
-app.delete('/del_student', function(req, res) {
-    console.log('Got a DELETE request for the delete page');
+app.delete('/del_student', function(req, res){
+    console.log('Got a delete request for the delete page');
     res.send('The Page got deleted');
 });
 
-// GET Request
-app.get('/enrolled_student', function(req, res) {
-    console.log('Got a GET request from enrolled student page');
+app.get('/enrolled_student', function(req, res){
+    console.log('Got a get request from enrolled student page');
     res.send('I am enrolled student');
 });
 
-// Server
-var server = app.listen(PORT, function() {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+var server= app.listen(3000,function(){
+    var port=server.address().port
+    console.log('Example app listening at http://localhost:%s',port)
+})
