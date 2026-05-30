f944dbffc06b5fefc0f8957e7e21e34c03b051a0
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-30 19:41:55 +0530

    init

--- U3/bootstrap_app/src/index.js
+++ U3/bootstrap_app/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './components/Portal_Navigation';
 // import App from './components/Student_Profile';
-import App from './components/Online_Learning';
+import Student from './Student';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Student />
   </React.StrictMode>
 );
 reportWebVitals();
