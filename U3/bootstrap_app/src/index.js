631765559b7cd6328d66e8ad44125e502c89a374
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-30 19:09:18 +0530

    init

--- U3/bootstrap_app/src/index.js
+++ U3/bootstrap_app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Portal_Navigation';
+// import App from './components/Portal_Navigation';
+import App from './components/Student_Profile';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
