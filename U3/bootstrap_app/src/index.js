eed7b9e7af57408bccebdb8a5eff2fb00f19a61e
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-30 19:12:41 +0530

    init

--- U3/bootstrap_app/src/index.js
+++ U3/bootstrap_app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './components/Portal_Navigation';
 // import App from './components/Student_Profile';
-import Product from "./components/Product_Search";
+import Product from './components/Product_Search';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
