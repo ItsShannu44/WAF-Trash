47ba8af3f954e5f02ef42a7ce65ef2f26db95b02
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-05-30 18:51:16 +0530

    init

--- U3/bootstrap_app/src/index.js
+++ U3/bootstrap_app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './sampleApp';
+import App from './Portal_Navigation.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
