671919d3052f4040df84623fd6dd7b76ad7dee0b
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-03 14:50:20 +0530

    new updates

--- U3/bootstrap_app/src/index.js
+++ U3/bootstrap_app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './components/Portal_Navigation';
 // import App from './components/Student_Profile';
-import Student from './Student';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
