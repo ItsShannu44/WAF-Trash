3b2c233cc803ffb8787deda99cfa626b95d8be62
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-04 10:01:46 +0530

    new updates

---
+++ U4/Rest_API/restapi/src/components/ApiExample.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+
+const ApiExample =() =>
+{
+    const [data, setData]=useState([]);
+    useEffect(() =>
+    {
+        const fetchData =async () =>
+        {
+            try{
+                const response = await
+                
+            }
+        }
+    })
+}
