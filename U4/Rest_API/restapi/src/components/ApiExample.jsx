20abb294992f26899fecbd686b2b69dd9e67b066
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-04 10:05:02 +0530

    new updates

--- U4/Rest_API/restapi/src/components/ApiExample.jsx
+++ U4/Rest_API/restapi/src/components/ApiExample.jsx
@@ -9,7 +9,9 @@ const ApiExample =() =>
         {
             try{
                 const response = await
-                
+                fetch("https://jsonplaceholder.typicode.com/users");
+                const result = await response.json();
+                setData(result);
             }
         }
     })
