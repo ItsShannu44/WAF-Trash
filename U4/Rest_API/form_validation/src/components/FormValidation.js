c9a0865e7b912fe8e7274b35f1ee118aeef54062
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-08 14:37:47 +0530

    updates

--- U4/Rest_API/form_validation/src/components/FormValidation.js
+++ U4/Rest_API/form_validation/src/components/FormValidation.js
@@ -3,7 +3,25 @@ import {useForm} from "react-hook-form";
 
 function FormValidation({addStudent})
 {
-    
-    
+        const{
+            register,
+            handleSubmit,
+            reset,
+            formState: {errors}
+        }= useForm();
+
+        const onSubmit = (data) =>{
+            addStudent(data.username);
+            alert("Form Submitted Successfully")
+            console.log(data);
+            reset();
+        };
+        
+        const addStudent =(studentName) =>
+        {
+            id: students.length+1,
+            name: studentName
+        }
+
 }
 export default FormValidation;
