b7ccb9873ce00e04d9c245a21b8fec5b2096dd37
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-08 15:54:44 +0530

    updates

---
+++ U4/Script/Sample_1.js
@@ -0,0 +1,15 @@
+const {MongoClient} = require('mongodb');
+
+const url = 'mongodb://127.0.0.1:27017';
+
+const dbName = 'nodeDB';
+const collectionName = "emp-collection";
+
+const client = new MongoClient(url);
+
+async function main()
+{
+    try{
+        
+    }
+}
