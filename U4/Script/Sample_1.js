3e3ac2ff98f62fd1dab67727d6b55c4fad1a6e3b
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-08 16:00:46 +0530

    updates

--- U4/Script/Sample_1.js
+++ U4/Script/Sample_1.js
@@ -10,6 +10,25 @@ const client = new MongoClient(url);
 async function main()
 {
     try{
-        
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
+
+        const db = client.db(dbName);
+
+        const collection=db.collection(collectionName);
+        const insertResult =
+        await collection.insertOne({ id : 1, name: 'Tom Holland' , age : 24});
+        console.log("Document inserted:", insertResult.insertedId);
+
+        const documents = await collection.find({}). toArray();
+        console.log('Documents found:', documents);
+    }
+    catch(err)
+    {
+        console.error("An error occured:", err);
+    }
+    finally{
+        await client.close();
     }
 }
+main().catch(console.error);
