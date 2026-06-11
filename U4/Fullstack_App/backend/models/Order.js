22c3221d6ac57e807cbecc2487b725ffa489c1a9
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-06-11 10:43:00 +0530

    init_updates

---
+++ U4/Fullstack_App/backend/models/Order.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+    product: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required : true
+    },
+    total: {
+        type: Number,
+        required : true
+    }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
--- U4/Fullstack_App/backend/routes/orderRoutes.js
+++ U4/Fullstack_App/backend/routes/orderRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Product = require("../model/Product");
+const Order = require("../models/Order");
 
+router.post("/products", async(req, res)=>{
+    try{
+        const product = await Product.create(req.body);
+        res.json(product);
+    }
+    catch(error)
+    {
+        
+    }
+})
 router.get('/', (req, res) => {
     res.send('Orders Route');
 });
